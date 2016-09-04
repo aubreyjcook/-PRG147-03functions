b45c1ea621d4e54574ef213d414d039e06260d9a
Aubrey Cook <aubreyjcook@gmail.com>
2016-09-03 23:46:27 -0500

    complete

---
+++ js/javascript.js
@@ -0,0 +1,54 @@
+function change01(){
+    document.getElementById("large").src="images/picture01.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change02(){
+    document.getElementById("large").src="images/picture02.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change03(){
+    document.getElementById("large").src="images/picture03.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change04(){
+    document.getElementById("large").src="images/picture04.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change05(){
+    document.getElementById("large").src="images/picture05.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change06(){
+    document.getElementById("large").src="images/picture06.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change07(){
+    document.getElementById("large").src="images/picture07.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change08(){
+    document.getElementById("large").src="images/picture08.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change09(){
+    document.getElementById("large").src="images/picture09.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change10(){
+    document.getElementById("large").src="images/picture10.jpg";
+    document.getElementById("large").alt="";
+}
+
+function change11(){
+    document.getElementById("large").src="images/picture11.jpg";
+    document.getElementById("large").alt="";
+}
